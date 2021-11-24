9ee0ad0e561ce4c18285202efb3e67c3b5a59a4f
Francesco <ffrancescogenovese@gmail.com>
2021-11-25 00:42:49 +0100

    Fixes

--- app.py
+++ app.py
@@ -6,13 +6,18 @@ import requests as r
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 @app.route("/", methods = ['POST', 'GET'])
 def home():
+    return "Hello world"
+
+@app.route("/aule", methods = ['POST', 'GET'])
+def aule():
     t = datetime.datetime.now()
     datestr = str(t.day)+ "/" + str(t.month)+ "/" + str(t.year)+ " " + str(t.hour) + ":" + str(t.minute);
     return render_template('index.html', edifici=edifici, date=datestr)
 
-@app.route('/results', methods = ['POST', 'GET'])
+@app.route('/aule/results', methods = ['POST', 'GET'])
 def data():
     if request.method == 'GET':
         return f"The URL /data is accessed directly. Try going to '/form' to submit form"
