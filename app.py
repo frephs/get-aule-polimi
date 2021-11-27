2b7949010c97a9200be1f8e187ad95c9a6e60c70
Francesco <ffrancescogenovese@gmail.com>
2021-11-27 23:55:31 +0100

    ssl

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def data():
 
 @app.before_request
 def before_request():
-    if not request.is_secure:
+    if not request.is_secure and '--secure' in sys.argv:
         url = request.url.replace('http://', 'https://', 1)
         code = 301
         return redirect(url, code=code)
